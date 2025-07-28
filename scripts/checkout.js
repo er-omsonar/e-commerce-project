3e2cb743c37d1f2c6a3bb9f48c0b938a25b9a821
er-omsonar <er.omsonar@gmail.com>
2025-07-28 16:33:38 +0530

    Delete products from HTML when click delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -12,7 +12,8 @@ cart.forEach((cartItem)=>{
   });
   
   cartSummaryHtml+=`
-  <div class="cart-item-container">
+  <div class="cart-item-container 
+  js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Wednesday, June 15
       </div>
@@ -94,6 +95,9 @@ document.querySelectorAll('.js-delete-link')
   link.addEventListener('click',()=>{
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
 });
