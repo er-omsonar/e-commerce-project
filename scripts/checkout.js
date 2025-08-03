98db8cc8351d3d7ca3911e80b9f2acc7d18e89dd
er-omsonar <er.omsonar@gmail.com>
2025-08-03 15:54:12 +0530

    making checkout(items) interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,16 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { cart } from "../data/cart.js";
 renderOrderSummary();
 renderPaymentSummary();
+function updateCartQuantity() {
+  let cartQuantity = 0;
+cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+    
+  });
+
+  document.querySelector('.js-return-to-home-link')
+    .innerHTML = `${cartQuantity} items`;
+}
+updateCartQuantity();
